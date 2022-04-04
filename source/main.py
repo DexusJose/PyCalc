28cc1aa3788b9ba1b7b2a2011ab6a1d0fab31579
DexusJose <jose_perezmiranda@outlook.com>
2022-04-03 20:15:00 -0500

    Creacion de la gui. *Creacuion de la gui y la distribucion del frame.

--- source/main.py
+++ source/main.py
@@ -1,8 +1,14 @@
 #This program is only for test
 
+import gui as g
+
 class Mainapp:
     def __init__(self):
-        print("App stated")
+        
+        print("App started")
+        appGui = g.Guiaplication()
+        appGui
+        
 
 Aplication = Mainapp()
 Aplication
